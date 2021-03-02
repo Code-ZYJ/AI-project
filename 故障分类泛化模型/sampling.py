7d3f1bd525e1865b55d1fa4631247c74a664c52d
Code-ZYJ <1103447068@qq.com>
2021-03-02 15:05:44 +0800

    init

---
+++ 故障分类泛化模型/sampling.py
@@ -0,0 +1,16 @@
+import numpy as np
+from torch import nn
+import torch
+'''
+使用前确保最后一维为采样的维度
+'''
+def sampling(data, size=1024):
+    data = torch.from_numpy(data).type(torch.float)
+    out = nn.Upsample(size=size)(data)
+    out = np.array(out)
+    return out
+
+if __name__=='__main__':
+    data = np.random.random((1,3,5006))
+    sampling(data).shape
+
